435df2cfa27de8d2a1c52c3f4cd45962d1f79a14
ajahdailey <42683466+ajahdailey@users.noreply.github.com>
2019-02-06 22:21:20 -0500

    Add files via upload

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,46 @@
  
-
+import java.util.*;
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        for (int i = 0; i < numberOfStars; i++) {
+         row = row + "*";
+        } 
+        return row ;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= numberOfRows; i++) {  
+            for(int j = 0; j < i ; j++) { 
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle ;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= 4; i++) {  
+            for(int j = 0; j < i ; j++) { 
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle ;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= 9; i++) {  
+            for(int j = 0; j < i ; j++) { 
+                triangle = triangle + "*";
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle ;
     }
 }
